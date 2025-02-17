d8724cdcd93d023dfcba18d812fdaa1a52551518
Stanislav <stankhil@gmail.com>
2025-02-17 13:16:48 +0200

    port upd

--- backend/server.js
+++ backend/server.js
@@ -20,4 +20,6 @@ app.use("/api/expenses", expenseRoutes);
 app.use("/api/incomes", incomeRoutes);
 
 const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
+});
